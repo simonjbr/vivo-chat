81d932e4f52ae23b724050323e7d3759ea93db2b
Simon Brown <simonandon@live.com.au>
2024-07-06 12:02:38 +0800

    Created DB in MongoDB Atlas and configured connection

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import connectToMongoDb from './config/connection.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+	await connectToMongoDb();
 	console.log(`Server listening at port: ${PORT}`);
 });
