352a1325d6ddd318974bc428614791b14968d60e
Simon Brown <simonandon@live.com.au>
2024-07-09 09:20:46 +0800

    Added comments

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,21 @@
 import { User } from '../models/index.js';
 import bcrypt from 'bcrypt';
 
+// controller for /api/auth/signup
 export const signup = async (req, res) => {
-	console.log('signup');
+	try {
+		const { username, email, password, confirmPassword, avatar } = req.body;
+
+		// avatars generated with robohash.org/<username>?set=set<1-5>
+	} catch (error) {}
 };
 
+// controller for /api/auth/login
 export const login = async (req, res) => {
 	console.log('login');
 };
 
+// controller for /api/auth/logout
 export const logout = async (req, res) => {
 	console.log('logout');
 };
