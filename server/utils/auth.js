ecd8b673e06bd3286d8339e09d1077d5eb91346e
Simon Brown <simonandon@live.com.au>
2024-07-10 11:14:06 +0800

    Created auth middleware to add user data from verified tokens to context

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,6 +10,36 @@ export const AuthenticationError = new GraphQLError(
 	}
 );
 
+// auth middleware verifies token and adds user data to request object
+export const authMiddleware = ({ req }) => {
+	// get token from headers
+	let token = req.headers.authorization;
+
+	// remove 'Bearer ' from token string
+	if (req.headers.authorization) {
+		token = token.split(' ').pop().trim();
+	}
+
+	// if token empty return unaltered request object
+	if (!token) {
+		return req;
+	}
+
+	// verify token and extract user data
+	try {
+		const { data } = jwt.verify(token, process.env.JWT_SECRET, {
+			maxAge: process.env.TOKEN_EXP,
+		});
+		req.user = data;
+	} catch (error) {
+		console.log('Invalid token');
+		console.error(error);
+	}
+
+	// return updated request object so it can be passed to the resolver as context
+	return req;
+};
+
 export const signToken = ({ username, email, _id }) => {
 	const payload = { username, email, _id };
 	return jwt.sign({ data: payload }, process.env.JWT_SECRET, {
