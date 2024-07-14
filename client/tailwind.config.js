c2ad2bcb699bc2e282241be5563a0ab47a623754
Simon Brown <simonandon@live.com.au>
2024-07-14 11:43:10 +0800

    Added custom color palette to tailwind theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,24 @@ import daisyui from 'daisyui';
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+		},
+		extend: {
+			colors: {
+				'rich-black': '#031826',
+				moonstone: '#6A9CA0',
+				platinum: '#DFDFE2',
+				zomp: '#329A79',
+				claret: '#6A1127',
+				'mint-green': '#CCE0DA',
+				'tea-green': '#CCFBAF',
+				'lime-green': '#21D427',
+				'lime-greener': '#21FD27',
+				'steel-blue': '#4D7EA8',
+			},
+		},
 	},
 	plugins: [daisyui],
 };
