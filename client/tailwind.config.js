5d69f1ceedfe7cc980da19740339aeba61227a0e
Simon Brown <simonandon@live.com.au>
2024-07-14 12:38:26 +0800

    Added 'teko' and 'roboto' google fonts and a plugin to enable a text-shadow class

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
 import daisyui from 'daisyui';
+import plugin from 'tailwindcss/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
+	mode: 'jit',
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		colors: {
@@ -20,7 +22,28 @@ export default {
 				'lime-greener': '#21FD27',
 				'steel-blue': '#4D7EA8',
 			},
+			fontFamily: {
+				teko: ['Teko', 'serif'],
+				roboto: ['Roboto', 'serif'],
+			},
+			textShadow: {
+				sm: '0 0px 1px #21FD27',
+				DEFAULT: '1px 1px 3px #21FD27',
+				lg: '1px 1px 8px #21FD27',
+			},
 		},
 	},
-	plugins: [daisyui],
+	plugins: [
+		daisyui,
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme('textShadow') }
+			);
+		}),
+	],
 };
