f5dc971a806220901af617e20f63fe05811ac124
Simon Brown <simonandon@live.com.au>
2024-07-16 09:32:49 +0800

    Created Sidebar component

---
+++ client/src/components/sidebar/Chat.jsx
@@ -0,0 +1,26 @@
+const Chat = () => {
+	return (
+		<>
+			<div className="flex gap-2 items-center hover:bg-rich-black rounded p-2 py-1 cursor-pointer">
+				<div className="avatar online">
+					<div className="w-12 rounded-full">
+						<img
+							src="https://robohash.org/125.253.50.25.png?set=set1"
+							alt="user avatar"
+						/>
+					</div>
+				</div>
+
+				<div className="flex flex-col flex-1">
+					<div className="flex gap-3 justify-between">
+						<p className="font-bold text-mint-green">John Doe</p>
+						<span className="text-xl">{':]'}</span>
+					</div>
+				</div>
+			</div>
+
+			<div className="divider my-0 py-0 h-1" />
+		</>
+	);
+};
+export default Chat;
---
+++ client/src/components/sidebar/Chats.jsx
@@ -0,0 +1,15 @@
+import Chat from './Chat';
+
+const Chats = () => {
+	return (
+		<div className="py-2 flex flex-col overflow-auto">
+			<Chat />
+			<Chat />
+			<Chat />
+			<Chat />
+			<Chat />
+			<Chat />
+		</div>
+	);
+};
+export default Chats;
