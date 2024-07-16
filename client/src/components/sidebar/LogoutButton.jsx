f47cc929c45d3ae7ca7e126a020e7c69303aa7e9
Simon Brown <simonandon@live.com.au>
2024-07-16 09:33:36 +0800

    Added LogoutButton to Sidebar

---
+++ client/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,10 @@
+import { BiLogOut } from 'react-icons/bi';
+
+const LogoutButton = () => {
+	return (
+		<div className="mt-auto">
+			<BiLogOut className="w-6 h-6 text-mint-green cursor-pointer hover:text-lime-green hover:text-shadow-sm" />
+		</div>
+	);
+};
+export default LogoutButton;
