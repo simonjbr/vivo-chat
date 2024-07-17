f06ef229069088afc2318c0e714fcdfa1abf50ed
Simon Brown <simonandon@live.com.au>
2024-07-17 09:46:16 +0800

    Wrapped App in BrowserRouter and placed routes to Home, Login and Signup in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
-// import Login from './pages/login/Login';
-// import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
 	return (
 		<div className=" bg-gradient-to-tr from-rich-black to-new-slate p-4 min-h-[100svh] flex items-center justify-center">
-			<Home />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={<Signup />} />
+			</Routes>
 		</div>
 	);
 }
