51160726d19fe0e2abd135b666a8bd142574567f
Simon Brown <simonandon@live.com.au>
2024-07-18 11:47:59 +0800

    Created useSignup hook to provide a signup function that will call addUser mutation, set user data in local storage for persistance, and update AuthContext with the new user's data

---
+++ client/src/hooks/useSignup.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+import { useAuthContext } from '../context/AuthContext';
+
+const useSignup = ({ username, password, confirmPassword, avatar }) => {
+	const [loading, setLoading] = useState(false);
+	const { setAuthUser } = useAuthContext();
+	const [addUser] = useMutation(ADD_USER, {
+		variables: {
+			username,
+			password,
+			confirmPassword,
+			avatar,
+		},
+	});
+
+	const signup =
+		async (/* { username, password, confirmPassword, avatar } */) => {
+			const isValidInput = validateInput(
+				username,
+				password,
+				confirmPassword,
+				avatar
+			);
+			if (!isValidInput) {
+				return;
+			}
+
+			setLoading(true);
+			try {
+				const { data, error } = await addUser(
+					username,
+					password,
+					confirmPassword,
+					avatar
+				);
+				console.log(data.addUser.user._id);
+				if (error) {
+					throw new Error(error);
+				}
+
+				// persist authUser in localStorage
+				localStorage.setItem(
+					'vivo-user',
+					JSON.stringify(data.addUser.user)
+				);
+
+				// add user to context
+				setAuthUser(data.addUser.user);
+			} catch (error) {
+				console.log(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+	return { loading, signup };
+};
+
+const validateInput = (username, password, confirmPassword, avatar) => {
+	if (!username || !password || !confirmPassword || !avatar) {
+		toast.error('Please fill in all fields');
+		return false;
+	}
+
+	if (password !== confirmPassword) {
+		toast.error('Password does not match Confirm Password');
+		return false;
+	}
+
+	if (password.length < 6) {
+		toast.error('Password must be at least 6 characters');
+		return false;
+	}
+
+	return true;
+};
+
+export default useSignup;
