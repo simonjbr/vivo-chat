04140666d26087de45865d6584263369ef9ef1f0
Simon Brown <simonandon@live.com.au>
2024-07-18 19:33:13 +0800

    Used useGetChats hook to dynamically render Chat components for each chat. Populated Chat component with data from chat prop and set up isSelected functionality

--- client/src/components/sidebar/Chat.jsx
+++ client/src/components/sidebar/Chat.jsx
@@ -1,25 +1,35 @@
-const Chat = () => {
+import useChatStore from '../../store/useChatStore';
+
+const Chat = ({ chat, lastIndex }) => {
+	const { selectedChat, setSelectedChat } = useChatStore();
+
+	const isSelected = selectedChat?._id === chat._id;
+
 	return (
 		<>
-			<div className="flex gap-2 items-center hover:bg-rich-black rounded p-2 py-1 cursor-pointer">
+			<div
+				className={`flex gap-2 items-center hover:bg-rich-black rounded p-2 py-1 cursor-pointer ${
+					isSelected ? 'bg-rich-black' : ''
+				}`}
+				onClick={() => setSelectedChat(chat)}
+			>
 				<div className="avatar online">
 					<div className="w-12 rounded-full">
-						<img
-							src="https://robohash.org/125.253.50.25.png?set=set1"
-							alt="user avatar"
-						/>
+						<img src={chat.avatar} alt="user avatar" />
 					</div>
 				</div>
 
 				<div className="flex flex-col flex-1">
 					<div className="flex gap-3 justify-between">
-						<p className="font-bold text-mint-green">John Doe</p>
+						<p className="font-bold text-mint-green">
+							{chat.username}
+						</p>
 						<span className="text-xl">{':]'}</span>
 					</div>
 				</div>
 			</div>
 
-			<div className="divider my-0 py-0 h-1" />
+			{lastIndex ? '' : <div className="divider my-0 py-0 h-1" />}
 		</>
 	);
 };
--- client/src/components/sidebar/Chats.jsx
+++ client/src/components/sidebar/Chats.jsx
@@ -1,15 +1,25 @@
+import useGetChats from '../../hooks/useGetChats';
 import Chat from './Chat';
 
 const Chats = () => {
+	const { loading, chats } = useGetChats();
+	console.log(chats);
+
 	return (
 		<div className="py-2 flex flex-col overflow-auto">
-			<Chat />
-			<Chat />
-			<Chat />
-			<Chat />
-			<Chat />
-			<Chat />
+			{loading ? (
+				<span className="loading loading-spinner mx-auto"></span>
+			) : (
+				chats.map((chat, index) => (
+					<Chat
+						key={chat._id}
+						chat={chat}
+						lastIndex={index === chats.length - 1}
+					/>
+				))
+			)}
 		</div>
 	);
 };
+
 export default Chats;
