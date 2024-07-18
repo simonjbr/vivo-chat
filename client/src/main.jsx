75eaa7bf289234de72b0c8fe5e9dbbefdda43ba1
Simon Brown <simonandon@live.com.au>
2024-07-18 11:43:26 +0800

    Created AuthContext provider to provide logged in user info. Wrapped App with AuthProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 const client = new ApolloClient({
 	uri: '/graphql',
@@ -14,7 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<ApolloProvider client={client}>
 			<BrowserRouter>
-				<App />
+				<AuthProvider>
+					<App />
+				</AuthProvider>
 			</BrowserRouter>
 		</ApolloProvider>
 	</React.StrictMode>
