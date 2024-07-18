d8343f1aacec1368022ab15ba00fdc30a176387c
Simon Brown <simonandon@live.com.au>
2024-07-18 09:22:03 +0800

    Setup Apollo client and wrapped App with ApolloProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+	uri: '/graphql',
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ApolloProvider>
 	</React.StrictMode>
 );
