a50f3012f13144837146c0edc477a85b7da9a693
Simon Brown <simonandon@live.com.au>
2024-07-18 09:22:49 +0800

    Setup react-hot-toast in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,17 @@ import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 	return (
 		<div className=" bg-gradient-to-tr from-rich-black to-new-slate p-4 min-h-[100svh] flex items-center justify-center">
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/signup' element={<Signup />} />
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
 			</Routes>
+			<Toaster />
 		</div>
 	);
 }
