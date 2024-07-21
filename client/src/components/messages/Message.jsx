030960c213f70dfcfd90000aa537229781ea4da7
Simon Brown <simonandon@live.com.au>
2024-07-21 13:48:31 +0800

    Messages component now dynamically renders messages belonging to the currently selected Chat

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -1,19 +1,26 @@
-const Message = () => {
+import dayjs from 'dayjs';
+import { useAuthContext } from '../../context/AuthContext';
+
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const formattedTimestamp = dayjs
+		.unix(message.createdAt / 1000)
+		.format('hh:mm a');
+	const isFromMe = message.senderId._id === authUser._id;
+
+	console.log(authUser);
 	return (
-		<div className="chat chat-end">
+		<div className={`chat ${isFromMe ? 'chat-end' : 'chat-start'}`}>
 			<div className="chat-image avatar">
 				<div className="w-10 rounded-full">
-					<img
-						src="https://robohash.org/125.253.50.25.png?set=set1"
-						alt="user avatar"
-					/>
+					<img src={message.senderId.avatar} alt="user avatar" />
 				</div>
 			</div>
-			<div className="chat-bubble text-white bg-steel-blue">
-				Hi! What is upp?
+			<div className={`chat-bubble text-white ${isFromMe ? 'bg-steel-blue' : 'bg-new-slate'}`}>
+				{message.content}
 			</div>
 			<div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-				12:42
+				{formattedTimestamp}
 			</div>
 		</div>
 	);
--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,21 +1,27 @@
+import { useQuery } from '@apollo/client';
+import useChatStore from '../../store/useChatStore';
 import Message from './Message';
+import { MESSAGES } from '../../utils/queries';
+import { useEffect, useState } from 'react';
 
 const Messages = () => {
+	const { selectedChat } = useChatStore();
+	const { data, error, loading } = useQuery(MESSAGES, {
+		variables: {
+			receiverId: selectedChat._id,
+		},
+	});
+
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		if (data) {
+			setMessages(data.messages);
+		}
+	}, [data]);
 	return (
 		<div className="px-4 flex-1 overflow-auto">
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
-			<Message />
+			{messages.map((message) => (<Message key={message._id} message={message} />))}
 		</div>
 	);
 };
