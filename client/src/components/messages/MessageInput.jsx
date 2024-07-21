1ec341287905b05a80951b4b24e5381ad7363eee
Simon Brown <simonandon@live.com.au>
2024-07-21 11:27:06 +0800

    Created hook for sending messages and implemented it in MessageInput submit

--- client/src/components/messages/MessageInput.jsx
+++ client/src/components/messages/MessageInput.jsx
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 import { BiSend } from 'react-icons/bi';
+import useSendMessage from '../../hooks/useSendMessage';
+import useChatStore from '../../store/useChatStore';
 
 const MessageInput = () => {
+	const [messageInput, setMessageInput] = useState('');
+	const { selectedChat } = useChatStore();
+
+	const receiverId = selectedChat?._id;
+	const { loading, sendMessage } = useSendMessage({
+		receiverId,
+		content: messageInput,
+	});
+
+	const handleMessageSubmit = async (e) => {
+		e.preventDefault();
+
+		const success = await sendMessage();
+
+		if (success) {
+			setMessageInput('');
+		}
+	};
 	return (
-		<form className="px-4 my-3">
+		<form className="px-4 my-3" onSubmit={handleMessageSubmit}>
 			<div className="w-full flex">
 				<input
 					type="text"
+					name="messageInput"
+					value={messageInput}
+					onChange={(e) => setMessageInput(e.target.value)}
 					className="border text-sm rounded-lg block w-full p-2.5 bg-new-slate focus:bg-rich-black"
 				/>
 				<button
 					type="submit"
 					className="inset-y-0 end-0 flex items-center ps-2"
+					disabled={loading}
 				>
-					<BiSend className='text-2xl text-mint-green cursor-pointer hover:text-lime-green' />
+					{loading ? <span className='loading loading-spinner'></span> : <BiSend className="text-2xl text-mint-green cursor-pointer hover:text-lime-green" />}
 				</button>
 			</div>
 		</form>
