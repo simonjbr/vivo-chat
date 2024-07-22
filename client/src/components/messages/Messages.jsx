a7b56a239851dc5064425404534340a6ec337380
Simon Brown <simonandon@live.com.au>
2024-07-22 09:59:45 +0800

    Now providing senderId for messages query. Also conditionaly rendering message skeleton while loading messages or an info paragraph if there are no messages

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -3,25 +3,40 @@ import useChatStore from '../../store/useChatStore';
 import Message from './Message';
 import { MESSAGES } from '../../utils/queries';
 import { useEffect, useState } from 'react';
+import MessageSkeleton from '../skeleton/MessageSkeleton';
+import { useAuthContext } from '../../context/AuthContext';
 
 const Messages = () => {
 	const { selectedChat } = useChatStore();
+	const { authUser } = useAuthContext();
 	const { data, error, loading } = useQuery(MESSAGES, {
 		variables: {
 			receiverId: selectedChat._id,
+			senderId: authUser._id,
 		},
 	});
 
 	const [messages, setMessages] = useState([]);
 
 	useEffect(() => {
+		if (error) {
+			console.log(error.message);
+		}
 		if (data) {
 			setMessages(data.messages);
 		}
-	}, [data]);
+	}, [data, error]);
 	return (
 		<div className="px-4 flex-1 overflow-auto">
-			{messages.map((message) => (<Message key={message._id} message={message} />))}
+			{loading ? (
+				[...Array(2)].map((_, index) => <MessageSkeleton key={index} />)
+			) : messages.length === 0 ? (
+				<p className="text-center">Send a message to start the chat</p>
+			) : (
+				messages.map((message) => (
+					<Message key={message._id} message={message} />
+				))
+			)}
 		</div>
 	);
 };
