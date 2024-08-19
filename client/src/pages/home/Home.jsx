db165e380f4b38f34114db8d4fb1e378c0de2847
Simon Brown <simonandon@live.com.au>
2024-08-19 10:33:38 +0800

    Altered breakpoints and general ui for imporved mobile experience

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import { OnlineUserContextProvider } from '../../context/OnlineUserContext.jsx';
 const Home = () => {
 	return (
 		<OnlineUserContextProvider>
-			<div className="flex h-[97svh] sm:h-[550px] rounded-lg overflow-hidden bg-steel-blue bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-15">
+			<div className="flex h-[90vh] max-lg:w-[90vw] lg:w-[1000px] rounded-lg overflow-hidden bg-steel-blue bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-15">
 				<Sidebar />
 				<MessageContainer />
 			</div>
