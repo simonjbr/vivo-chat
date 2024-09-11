87a9e355c4e4213af97c247af6b6b8c046ee2848
Simon Brown <simonandon@live.com.au>
2024-09-11 11:34:09 +0800

    Set up connection to local development db in order to test changes to Chat participants field

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,13 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI);
+const connectionString =
+	process.env.NODE_ENV === 'development'
+		? 'mongodb://127.0.0.1:27017/vivoChat'
+		: process.env.MONGODB_URI;
+
+setTimeout(() => {
+	mongoose.connect(connectionString);
+}, 100);
 
 export default mongoose.connection;
