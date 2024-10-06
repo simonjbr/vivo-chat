d42e67382c022dabd69811c6db741b5dda7686a1
Simon Brown <simonandon@live.com.au>
2024-10-06 11:31:09 +0800

    Rearranged the mongoose connection configuration to make sure db.once function is available at the right time in server.js.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import mongoose from 'mongoose';
-
 const connectionString =
 	process.env.NODE_ENV === 'development'
 		? 'mongodb://127.0.0.1:27017/vivoChat'
 		: process.env.MONGODB_URI;
 
-setTimeout(() => {
-	mongoose.connect(connectionString);
-}, 100);
-
-export default mongoose.connection;
+export default connectionString;
